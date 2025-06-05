f57899eb30241fcdb495d34cb682a1b91b6ea878
Laura <laurarpulido9@gmail.com>
2025-06-06 01:20:39 +0200

    Cambio a php del index, header, footer

---
+++ index.php
@@ -0,0 +1,252 @@
+<?php include_once("./header.php")?>
+    <main>
+
+        <section id="banner" class=" container-fluid vh-100 position-relative">
+
+        <div class="textoBanner">
+
+            <h3 class="fs-5 fs-md-4">Nueva Colección</h3>
+            <h2 class="fs-3 fs-md-2">"Wiggle"</h2>
+
+       </div>
+        </section>
+
+        <section id="estilos" class="container-fluid py-3 my-3 py-lg-5 my-lg-5">
+            <div class="cartaEstilos">
+                <h3 class="text-center mb-4 tituloMedioNeg">Encuentra tu estilo</h3>
+                
+                <div class="row">
+                    <div class="col-6 col-sm-3 imageEstilo">
+                        <div class="imgContenedor">
+                            <img src="./img/estilos/iconY2K-min.png" alt="Estilo Y2K" class="img-fluid imagenCirculo">
+                            <h4 class="text-center mt-2">Y2K</h4>
+                        </div>
+                        
+                    </div>
+                    <div class="col-6 col-sm-3 imageEstilo">
+                        <div class="imgContenedor">
+                            <img src="./img/estilos/iconPunk-min.jpg" alt="Estilo Punk" class="img-fluid imagenCirculo">
+                            <h4 class="text-center mt-2">Punk</h4>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-3 imageEstilo">
+                        <div class="imgContenedor">
+                            <img src="./img/estilos/iconGoth-min.jpg" alt="Estilo Goth" class="img-fluid imagenCirculo">
+                            <h4 class="text-center mt-2">Goth</h4>
+                        </div>
+                    </div>
+                    <div class="col-6 col-sm-3 imageEstilo">
+                        <div class="imgContenedor">
+                            <img src="./img/estilos/iconEmo-min.jpg" alt="Estilo Scene / Emo" class="img-fluid imagenCirculo">
+                            <h4 class="text-center mt-2">Scene / Emo</h4>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+
+
+        <section class="container-fluid py-3 py-lg-5" id="novedades">
+
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="tituloGrand pb-3">Novedades</h3>
+                </div>
+            </div>
+        
+            <div class="row g-4"> 
+                
+                <div class="col-12 col-md-6">
+                    <div class="novedadCard colectionAltGore">
+                        <div class="textoNov">
+                            <h4>Colección: "Alt Gore"</h4>
+                        </div>
+                    </div>
+                </div>
+        
+                
+                <div class="col-12 col-md-6">
+                    <div class="novedadCard noticia2000">
+                        <div class="textoNov">
+                            <h4>El efecto 2000 de la moda</h4>
+                            <p>EL Y2K es el nombre del temido fallo informático...</p>
+                        </div>
+                    </div>
+                </div>
+        
+                
+                <div class="col-12 col-md-6">
+                    <div class="novedadCard noticiaScene">
+                        <div class="textoNov">
+                            <h4>Vuelve 'scene'</h4>
+                            <p>Un puñado de 'influencers' y antihéroes...</p>
+                        </div>
+                    </div>
+                </div>
+        
+                
+                <div class="col-12 col-md-6">
+                    <div class="novedadCard colectionGirlz">
+                        <div class="textoNov">
+                            <h4>Colección: "Girlz Have Fun"</h4>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row py-3 py-md-4">
+
+                <div class="col-5 col-md-2 text-center m-auto botonNov">
+
+                    <a href="#">Ir a novedades</a>
+
+                </div> 
+
+             </div> 
+        </section>
+
+        <section class="container-fluid py-3 py-lg-5" id="colecciones">
+
+            <div class="row">
+                <div class="col">
+                    <h3 class="tituloGrandNeg pb-3">Colecciones</h3>
+                </div>
+            </div>
+
+            <div class="row fondoOscuro pb-3 pb-md-4 pt-5 "> 
+                
+                <div class="col-12 col-lg-6">
+                    <div class="coleccionCard colectionRazzmatazz">
+                        <div class="textoCol">
+                            <h4>Colección: "Alt Gore"</h4>
+                        </div>
+                    </div>
+                </div>
+        
+                
+                <div class="col-12 col-lg-6">
+                    <div class="coleccionCard colectionWiggle">
+                        <div class="textoCol">
+                            <h4>El efecto 2000 de la moda</h4>
+                        </div>
+                    </div>
+                </div>
+        
+                
+                <div class="col-12 col-lg-6">
+                    <div class="coleccionCard colectionWilder">
+                        <div class="textoCol">
+                            <h4>Vuelve 'scene'</h4>
+                        </div>
+                    </div>
+                </div>
+        
+                
+                <div class="col-12 col-lg-6">
+                    <div class="coleccionCard colectionCorpse">
+                        <div class="textoCol">
+                            <h4>Colección: "Girlz Have Fun"</h4>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row fondoOscuro pb-5">
+
+                <div class="col-6 col-md-2 text-center m-auto botonCol">
+
+                    <a href="#">Ir a colecciones</a>
+
+                </div> 
+
+             </div> 
+
+        </section>
+
+        <section id="diseñadores" class="container-fluid py-3 py-lg-5">
+            <div class="cartaDiseñadores ">
+                <h2 class="text-center mb-5 tituloMedioNeg">Diseñadores</h2>
+                
+                <div class="row justify-content-center py-4">
+                    
+                    <div class="col-9 col-sm-4 col-lg-3 mb-4 diseñador">
+                        <div class="imgContenedor">
+                            <img src="./img/diseñadorEmilio.jpg" alt="Emilio Fernandez Diseñador" class="img-fluid imagenCirculo">
+                        </div>
+                        <div class="d-flex flex-column justify-content-evenly infoDis">
+                            <h3 class="text-center">Emilio Fernandez</h3>
+                            <div class="descripcion">
+                                <p class="text-center px-3">"Rompiendo esquemas con diseños audaces y tejidos vanguardistas. La moda es arte y yo pinto con tela."</p>
+                            </div>
+                            <div class="">
+                                <p class="text-center px-3">Me gusta: <span class="likesDis">130</span> <i class="bi bi-heart-fill"></i></p>
+                            </div>
+
+                        </div>
+                      
+                    </div>
+
+                     <div class="col-9 col-sm-4 col-lg-3 mb-4 diseñador">
+                        <div class="imgContenedor">
+                            <img src="./img/diseñadorLunaG-min.jpg" alt="Luna G Diseñador" class="img-fluid imagenCirculo">
+                        </div>
+                        <div class="d-flex flex-column justify-content-evenly infoDis">
+                            <h3 class="text-center">Luna G</h3>
+                            <div class="descripcion">
+                                <p class="text-center px-3">Diseñadora de moda apasionada por crear piezas únicas que fusionan innovación y tradición. Cada diseño cuenta una historia ✨ #Moda #Diseño #Creatividad</p>
+                            </div>
+                            <div class="">
+                                <p class="text-center px-3">Me gusta: <span class="likesDis">130</span> <i class="bi bi-heart-fill"></i></p>
+                            </div>
+
+                        </div>
+                      
+                    </div>
+                    
+                    
+                    <div class="col-9 col-sm-4 col-lg-3 mb-4 diseñador">
+                        <div class="imgContenedor">
+                            <img src="./img/diseñadorDavina-min.jpg" alt="Davina Diseñador" class="img-fluid imagenCirculo">
+                        </div>
+                        <div class="d-flex flex-column justify-content-evenly infoDis">
+                            <h3 class="text-center">Davina Dylan</h3>
+                            <div class="descripcion">
+                                <p class="text-center px-3">"Rompiendo reglas con diseños punk, rasgaduras intencionales y un toque gótico. La moda debe incomodar. 💀✂️"</p>
+                            </div>
+                            <div class="">
+                                <p class="text-center px-3">Me gusta: <span class="likesDis">130</span> <i class="bi bi-heart-fill"></i></p>
+                            </div>
+
+                        </div>
+                      
+                    </div>
+                    
+                    
+                   
+                </div>
+
+            </div>
+
+            <div class="row py-3 py-md-4">
+
+                <div class="col-5 col-md-2 text-center m-auto botonNov">
+
+                    <a href="#">Ir a diseñadores</a>
+
+                </div> 
+
+             </div> 
+
+
+        </section>
+
+        <section id="statement" class="mb-4">
+            <p>Alter:Gate es un portal archivo dedicado a diseñadores y tiendas de moda alternativa, sirviendo como una plataforma de referencia para la comunidad underground. Desde marcas emergentes hasta creadores icónicos, el sitio recopila información sobre diseñadores, colecciones y espacios de venta que definen estilos como el gótico, punk, cyber y más. Con un enfoque en la preservación y difusión de la moda no convencional, Alter:Gate es un recurso esencial para quienes buscan inspiración, conexiones y una ventana al mundo de la indumentaria alternativa.</p>
+        </section>
+
+    </main>
+
+<?php include_once("./footer.php")?>
+
