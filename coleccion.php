5f0e7dd5be04c42a5d87b2ae46293b9d032fc8d1
Laura <laurarpulido9@gmail.com>
2025-06-07 00:09:58 +0200

    Página diseñador

---
+++ coleccion.php
@@ -0,0 +1,67 @@
+<?php include_once("./header.php")?>
+
+    <section id="bannerColeccion" class=" container-fluid vh-100 position-relative" style="background: url(../img/imagenBanner.jpg) center center /cover ;">
+
+    </section>
+
+
+    <div id="selectorEstilo" class="container-fluid px-4 px-lg-5">
+            <div class="row d-flex align-items-center justify-content-center justify-content-md-between mt-5">
+                <div class="col-12 col-md-6 col-xl-4 mb-4 mb-md-4 ms-0 ms-lg-4">
+                    <h3 class="">Título efefee fefewe feffeff fewfef</h3>
+                </div>
+                <div class="col-9 col-md-5 col-xl-4 tarjetaDiseñador">
+                    
+                    <div class="imgContenedor">
+                        <img src="" alt="" class="img-fluid imagenCirculo">
+
+                    </div>
+
+                     <div class="col col-lg-auto col-xl my-3 my-lg-3 px-3 text-start ">
+                            <h4>Emilio Fernández</h4>
+                            
+                        <div class="col-12">
+                            
+                                <a class="botonNov">Ver Diseñador</span></a>
+                                
+                        </div>
+                    </div>
+                        
+
+                </div>
+            </div>
+        </div>
+
+    <div class="container-fluid">
+            <div class="row my-5">
+                <div class="col-6 col-lg-4 col-xl-3 columnaItem">
+                    <div class="contenedorImagenItem">
+                        <img src="./img/imgArticulos/camiseta-de-manga-larga-silver-heart-minga-london.jpg" alt="">
+                    </div>
+                    <p>Camiseta de Manga Larga Silver Heart - Minga London</p>
+
+                </div>
+                <div class="col-6 col-lg-4 col-xl-3 columnaItem">
+                    <div class="contenedorImagenItem">
+                        <img src="./img/imgArticulos/camiseta-de-manga-larga-silver-heart-minga-london.jpg" alt="">
+                    </div>
+                    <p>Camiseta de Manga Larga Silver Heart - Minga London</p>
+
+                </div>
+                <div class="col-6 col-lg-4 col-xl-3 columnaItem">
+                    <div class="contenedorImagenItem">
+                        <img src="./img/imgArticulos/camiseta-de-manga-larga-silver-heart-minga-london.jpg" alt="">
+                    </div>
+                    <p>Camiseta de Manga Larga Silver Heart - Minga London</p>
+
+                </div>
+
+                
+                
+            </div>
+            
+    </div>
+
+<?php include_once("./footer.php")?>
+
+
---
+++ diseñador.php
@@ -0,0 +1,153 @@
+<?php include_once("./header.php");?>
+
+    <section id="bannerDis">
+
+        <div class="container-fluid d-flex justify-content-center px-4">
+           
+                    <div class="container-fluid bannerDis px-5 py-2">
+                        <div class="row py-3 d-flex align-items-center flex-column flex-lg-column flex-xl-row justify-content-xl-between">
+                            <div class="col-auto">
+                                <div class="contenedorPerfil">
+
+                                    <img src="./img/diseñadorEmilio.jpg" alt="" class="imagenCirculo">
+                                </div>    
+                            </div>
+
+                            <div class="col  col-xl-6 my-3 perfilDis text-center text-lg-center text-xl-start">
+                                <h4>Emilio Fernández</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus doloremque temporibus animi, accusantium dolorum asperiores aut. Error earum excepturi voluptas tempore a! Dolor.</p>
+                            </div>
+
+                            <div class="d-flex col-12 col-xl-auto text-center text-xl-start justify-content-center gap-3">
+                                <div class="contContadores">
+                                    <p>Me gusta</p>
+                                    <h5>120<span></h5>
+                                    
+
+                                </div>
+                                <div class="contContadores">
+                                    <p>Artículos</p>
+                                    <h5>120</h5>
+                                    
+
+                                </div>
+                                <div class="contContadores">
+                                    <p>Colecciones</p>
+                                    <h5>120</h5>
+
+                                </div>
+
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+    </section>
+
+    <section id="desplegable">
+     
+    <div class="accordion px-3 px-xl-5 my-5" id="">
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne">
+            Colecciones
+          </button>
+        </h2>
+        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+          <div class="accordion-body container-fluid">
+            <div class="row d-flex justify-content-center g-4 m-0 p-0 "> 
+            
+            <div class="col-12 col-sm-6 col-xl-4 p-0 m-0">
+                <div class="coleccionCardDis" style="background-image: url(../img/colecciones/) center center /cover;">
+                    <div class="textoColDis">
+                        <h4>Colección: "Alt Gore"</h4>
+                    </div>
+                </div>
+            </div>
+    
+            
+            <div class="col-12 col-sm-6 col-xl-4 p-0 m-0">
+                <div class="coleccionCardDis" style="background: url(./img/colecciones/imagenColAlt.jpg) center center /cover;">
+                    <div class="textoColDis">
+                        <h4>El efecto 2000 de la moda</h4>
+                    </div>
+                </div>
+            </div>
+    
+            
+            <div class="col-12 col-sm-6 col-xl-4 p-0 m-0">
+                <div class="coleccionCardDis"style="background: url(../img/colecciones/) center center /cover;">
+                    <div class="textoColDis">
+                        <h4>Vuelve 'scene'</h4>
+                    </div>
+                </div>
+            </div>
+    
+            
+            <div class="col-12 col-sm-6 col-xl-4 p-0 m-0">
+                <div class="coleccionCardDis"style="background: url(../img/colecciones/imagenColAlt.jpg) center center /cover;">
+                    <div class="textoColDis">
+                        <h4>Colección: "Girlz Have Fun"</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+
+
+             </div> 
+        </div>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+            Artículos
+          </button>
+        </h2>
+        <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse show">
+          <div class="accordion-body">
+            <div class="container-fluid">
+            <div class="row my-5">
+                <div class="col-6 col-lg-4 col-xl-3 columnaItem">
+                    <div class="contenedorImagenItem">
+                        <img src="./img/imgArticulos/camiseta-de-manga-larga-silver-heart-minga-london.jpg" alt="">
+                    </div>
+                    <p>Camiseta de Manga Larga Silver Heart - Minga London</p>
+
+                </div>
+                <div class="col-6 col-lg-4 col-xl-3 columnaItem">
+                    <div class="contenedorImagenItem">
+                        <img src="./img/imgArticulos/camiseta-de-manga-larga-silver-heart-minga-london.jpg" alt="">
+                    </div>
+                    <p>Camiseta de Manga Larga Silver Heart - Minga London</p>
+
+                </div>
+                <div class="col-6 col-lg-4 col-xl-3 columnaItem">
+                    <div class="contenedorImagenItem">
+                        <img src="./img/imgArticulos/camiseta-de-manga-larga-silver-heart-minga-london.jpg" alt="">
+                    </div>
+                    <p>Camiseta de Manga Larga Silver Heart - Minga London</p>
+
+                </div>
+
+                
+                
+            </div>
+            
+            </div>
+          </div>
+        </div>
+        </div>
+    </div>
+    
+
+    </section>
+
+<?php include_once("./footer.php");?>
